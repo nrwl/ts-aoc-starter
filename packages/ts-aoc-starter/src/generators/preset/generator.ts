3c38cc8f4d10b4d02dd56472585a5e5c0c3cbf1b
Zack DeRose <zack.derose@gmail.com>
2023-11-30 20:24:58 -0500

    updating generated readme

--- packages/ts-aoc-starter/src/generators/preset/generator.ts
+++ packages/ts-aoc-starter/src/generators/preset/generator.ts
@@ -42,6 +42,7 @@ export async function presetGenerator(tree: Tree) {
   addModuleToPackageJson(tree);
   removeNxJson(tree);
   addPrettierRc(tree);
+  updateReadme(tree);
   await formatFiles(tree);
 }
 
@@ -147,6 +148,66 @@ function addPrettierRc(tree: Tree) {
 }
 `;
   tree.write('.prettierrc', content);
+}
+
+function updateReadme(tree: Tree) {
+  const content = `# ts-aoc-starter
+
+## Getting Started
+
+\`\`\`terminal
+npx create-ts-aoc-starter
+\`\`\`
+
+This will create a new workspace in the current directory with the following structure:
+
+\`\`\`file-tree
+ts-aoc-starter
+├── puzzles
+│   ├── day-1
+│   │   ├── day-1-a.data.txt
+│   │   ├── day-1-a.sample-data.txt
+│   │   ├── day-1-a.ts
+│   │   ├── day-1-b.data.txt
+│   │   ├── day-1-b.sample-data.txt
+│   │   └── day-1-b.ts
+│   ├── day-2
+│   ├── day-3
+\`\`\`
+
+## Running the Puzzles
+
+Copy and paste the sample data given in the problem into the \`day-X-a.sample-data.txt\` file.
+
+Copy and paste your larger unique actual data set into the \`day-X-a.data.txt\` file.
+
+Add your solution to the \`day-X-a.ts\` file.
+
+To run your solution against your sample data set, run the following command:
+
+\`\`\`terminal
+nx day-1-a --data=sample
+\`\`\`
+
+or
+
+\`\`\`terminal
+pnpm run day-1-a:sample
+\`\`\`
+
+To run your solution against your actual data set, run the following command:
+
+\`\`\`terminal
+nx day-1-a
+\`\`\`
+
+or
+
+\`\`\`terminal
+pnpm run day-1-a
+\`\`\`
+`;
+  tree.write('README.md', content);
 }
 
 function rmDashes(name: string): string {
